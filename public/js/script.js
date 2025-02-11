c6144a3ad8c833d0a6147c631ad4f09deed75a67
jarasa03 <javierarruabarrena03@gmail.com>
2025-02-11 20:51:44 +0100

    style: :art: Mejoras de estilo

--- public/js/script.js
+++ public/js/script.js
@@ -4,16 +4,16 @@ const columnas = ["a", "b", "c", "d", "e", "f", "g", "h"];
 for (let fila = 8; fila >= 1; fila--) {
     for (let col = 0; col < 8; col++) {
         const casilla = document.createElement("div");
+        casilla.classList.add("casilla")
 
         // Generar ID en formato "a1", "b2", etc.
-        let id = `${columnas[col]}${fila}`;
-        casilla.id = id;
+        casilla.id = columnas[col] + fila;
 
         // Asignar color según la posición
         if ((fila + col) % 2 === 0) {
-            casilla.classList.add("CasillaBlanca");
+            casilla.classList.add("casilla-blanca");
         } else {
-            casilla.classList.add("CasillaNegra");
+            casilla.classList.add("casilla-negra");
         }
 
         tablero.appendChild(casilla);
